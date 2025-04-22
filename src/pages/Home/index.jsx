63edde7776f6ae540982c6dc09e038c2ea03196b
Tulio <tulioluv@gmail.com>
2025-04-21 21:36:16 -0300

    feat: created Home component with API integration and CRUD functionalities

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState, useRef } from "react";
+import "./style.css";
+import api from "../../services/api";
+import Trash from "../../assets/Trash.png";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Home() {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const inputTask = useRef();
+
+  async function getTasks() {
+    const tasksFromApi = await api.get("/tasks");
+    setTasks(tasksFromApi.data);
+  }
+
+  async function createTasks() {
+    await api.post("/tasks", {
+      task: inputTask.current.value,
+    });
+
+    setInputValue("");
+    getTasks();
+  }
+
+  async function deleteTasks(id) {
+    await api.delete(`/tasks/${id}`);
+    getTasks();
+  }
+
+  useEffect(() => {
+    getTasks();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      <form>
+        <input
+          id="taskInput"
+          className="inputTask"
+          type="text"
+          placeholder="Add a task"
+          ref={inputTask}
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button
+          id="btn-Add"
+          type="button"
+          className="btn-Add"
+          onClick={createTasks}
+          disabled={!inputValue.trim()}
+          style={{
+            cursor: inputValue.trim() ? "pointer" : "not-allowed",
+            color: inputValue.trim() ? "#78BAFD" : "#605e5c",
+          }}
+        >
+          Add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </form>
+
+      {tasks.map((task) => (
+        <div className="tasks" key={task.id}>
+          <div className="task-content">
+            <div className="checkbox-wrapper-15">
+              <input
+                className="inp-cbx"
+                id={`cbx-${task.id}`}
+                type="checkbox"
+                style={{ display: "none" }}
+              />
+              <label className="cbx" htmlFor={`cbx-${task.id}`}>
+                <span>
+                  <svg width="12px" height="9px" viewBox="0 0 12 9">
+                    <polyline points="1 5 4 8 11 1"></polyline>
+                  </svg>
+                </span>
+                <span>{task.task}</span>
+              </label>
+            </div>
+          </div>
+          <button onClick={() => deleteTasks(task.id)} className="btn-Delete">
+            <img
+              src={Trash}
+              style={{ width: "16px", height: "16px" }}
+              alt="Delete"
+            />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default Home;
