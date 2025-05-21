83fb29a21b00478c9cfe6fa008e7df2d13a1746c
Tulio <tulioluv@gmail.com>
2025-05-21 15:39:04 -0300

    style: now using Tailwind CSS

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -32,21 +32,25 @@ function Home() {
   }, []);
 
   return (
-    <div className="container">
-      <form>
-        <input
-          id="taskInput"
-          className="inputTask"
-          type="text"
-          placeholder="Add a task"
-          ref={inputTask}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
+    
+    <main className="bg-[#121212] min-h-screen flex items-center justify-center px-4 py-10">
+  <form className="bg-[#1E1E2E] w-full rounded-md p-6 shadow-lg">
+    <div className="flex flex-col gap-4">
+      <input
+        id="taskInput"
+        className="bg-[#2A2A3C] h-12 text-[#E0E0E0] px-4 py-2 rounded-md placeholder-[#4bb1f1] focus:placeholder-white outline-none"
+        type="text"
+        placeholder="Add a task"
+        ref={inputTask}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+
+      <div className="flex justify-end">
         <button
           id="btn-Add"
           type="button"
-          className="btn-Add"
+          className="h-11 w-32 border border-[#2C2C3A] bg-[#3B3B5C] rounded-sm font-bold transition duration-300"
           onClick={createTasks}
           disabled={!inputValue.trim()}
           style={{
@@ -56,10 +60,15 @@ function Home() {
         >
           Add
         </button>
-      </form>
+      </div>
+    </div>
 
+    <div className="mt-6 space-y-4">
       {tasks.map((task) => (
-        <div className="tasks" key={task.id}>
+        <div
+          className="flex items-center justify-between bg-[#2A2A3C] p-4 rounded-md"
+          key={task.id}
+        >
           <div className="task-content">
             <div className="checkbox-wrapper-15">
               <input
@@ -74,11 +83,16 @@ function Home() {
                     <polyline points="1 5 4 8 11 1"></polyline>
                   </svg>
                 </span>
-                <span>{task.task}</span>
+                <span className="text-white font-semibold">{task.task}</span>
               </label>
             </div>
           </div>
-          <button onClick={() => deleteTasks(task.id)} className="btn-Delete">
+          
+          <button
+            type="button"
+            onClick={() => deleteTasks(task.id)}
+            className="btn-Delete hover:bg-[#3B3B3B] rounded p-2 cursor-pointer"
+          >
             <img
               src={Trash}
               style={{ width: "16px", height: "16px" }}
@@ -88,6 +102,9 @@ function Home() {
         </div>
       ))}
     </div>
+  </form>
+</main>
+
   );
 }
 
