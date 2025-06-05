2788ee6225a6d4cdbfad84c557623f8fb6e54bda
Tulio <tulioluv@gmail.com>
2025-06-05 20:07:21 -0300

    fix: adding url from render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://taskmanager-api-omfs.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
