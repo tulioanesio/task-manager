db299f0e57a4c4ff3c262c8ac24bbfd63f93d033
Tulio <tulioluv@gmail.com>
2025-06-16 13:05:34 -0300

    fix: render baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
-  //https://taskmanager-api-omfs.onrender.com
+  baseURL: "https://taskmanager-api-omfs.onrender.com",
+
 });
 
 api.interceptors.request.use((config) => {
